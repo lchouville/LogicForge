56059ff2941cb406023eb3563636028bbbab6c6a
luka <lu.chouville@AGORINFO.LAN>
2026-08-12 11:21:51 +0200

    feat: wire up the simulation, editor and rendering plugins

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+mod constants;
+mod editor;
+mod grid;
+mod rendering;
+mod simulation;
+
 use bevy::prelude::*;
+
+use editor::EditorPlugin;
+use rendering::RenderingPlugin;
+use simulation::SimulationPlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, hello_world)
+        .add_plugins((SimulationPlugin, EditorPlugin, RenderingPlugin))
         .run();
-}
-
-fn hello_world() {
-    info!("Hello, LogicForge!");
 }
